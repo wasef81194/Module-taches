e92261c959065b6f28bebf258e9202da187c34f4
Alexandra WASEF <alex.wasef@gmail.com>
2025-11-03 17:17:18 +0100

    make delete tache with jquery

--- taches/urls.py
+++ taches/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home,new
+from .views import home,new,delete
 
 urlpatterns = [
     path('', home, name='home'),
     path('taches/new', new, name='new'),
+    path('tache/delete', delete, name='delete'),
 ]
--- taches/views.py
+++ taches/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts  import render
 from .forms import TacheForm
+from .models import Tache
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def home(request):
-    return render(request,'taches/home.html')
+    taches = Tache.objects.all()
+    return render(request,'taches/home.html', {'taches' : taches})
 
 def new(request):
+    response = None
     form = TacheForm()
     if request.method == 'POST':
         form = TacheForm(request.POST)
@@ -15,4 +21,18 @@ def new(request):
             print('Enregistrement en bdd')
             tache.save()  # Enregistre en BDD !
             form = TacheForm() # Reset le formulaire
-    return render(request,'taches/new.html', {'form':form})
+            response =  {"bool": True, "message" : "Enregistrement éffectuer avec succées" }
+        else :
+            response =  {"bool": False, "message" : "Le formulaire n'est pas valide veuilliez saisir tout les champs correctement" } 
+    return render(request,'taches/new.html', {'form':form, 'response': response})
+
+@csrf_exempt
+def delete(request):
+    if request.method == 'POST':
+        objet_id = request.POST.get('id')  # Récupère l'id envoyé en POST
+        objet = get_object_or_404(Tache, id=objet_id)
+        objet.delete()
+        return JsonResponse({'success': True, 'message': 'Tache supprimé'})
+    else:
+        return JsonResponse({'success': False, 'message': 'Méthode non autorisée'}, status=405)
+    
